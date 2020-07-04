ea89f69935085d4320010887959d783519c3d3fd
Anoop Jangra <59556194+anoopjangra@users.noreply.github.com>
2020-07-04 16:32:45 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,23 +4,27 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 
+
 setup(
     name='pynse',
-    version='0.1',
+    version='0.1.0',
     packages=['pynse'],
     url='https://github.com/anoopjangra',
-    license='',
+    include_package_data=True,
+    package_data={'':['symbol_list/*']},
     author='Anoop Jangra',
     author_email='anoopjangra@gmail.com',
     description='Library to extract realtime and historical data from NSE website',
     long_description_content_type="text/markdown",
     long_description=long_description,
     install_requires=['requests', 'fake-headers', 'bs4', 'pandas','beautifulsoup4'],
+    keywords = ['nse','nsepy','stock markets','national stock exchange'],
     classifiers=[
       'Natural Language :: English',
       'Operating System :: OS Independent',
       'Programming Language :: Python',
       'Programming Language :: Python :: 3',
       'Programming Language :: Python :: Implementation :: PyPy',
-      'Topic :: Software Development :: Libraries :: Python Modules'
+      'Topic :: Software Development :: Libraries :: Python Modules',
+      'License :: OSI Approved :: MIT License'
     ],)
